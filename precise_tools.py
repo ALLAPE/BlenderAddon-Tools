73e2a529bd2af3676e8fbaa4062a3b854f588858
as <allensnape@qq.com>
2021-02-23 22:41:58 +0800

    添加边分割功能

--- precise_tools.py
+++ precise_tools.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 from mathutils import Vector
 
 bl_info = {
@@ -22,20 +23,19 @@ def get_selected_vertices(context):
     obj = context.active_object
     if obj.type != 'MESH':
         return list()
-    mode = context.active_object.mode
-    # we need to switch from Edit mode to Object mode so the selection gets updated
+    mode = obj.mode
     bpy.ops.object.mode_set(mode='OBJECT')
-    selected_vertices = [v for v in context.active_object.data.vertices if v.select]
-    # back to whatever mode we were in
+    selected_vertices = [v for v in obj.data.vertices if v.select]
     bpy.ops.object.mode_set(mode=mode)
     return selected_vertices
 
 
 # endregion
 
-# region origin tools
+# region ops
 
-class PutOriginToCenter(bpy.types.Operator):
+
+class PutOriginToCenterOp(bpy.types.Operator):
     """Set origin to the center of two selected vertices"""
     bl_idname = "object.to_the_center_of_two_selected_vertices"
     bl_label = "Set origin to the center"
@@ -76,9 +76,62 @@ class PutOriginToCenter(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class OriginToolsPanel(bpy.types.Panel):
-    """Origin Tools"""
-    bl_label = "Origin Tools"
+class SplitEdgesOp(bpy.types.Operator):
+    """Put a vertex between selected edges"""
+    bl_idname = "mesh.split_selected_edges_half"
+    bl_label = "Split selected edges half"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    position: bpy.props.FloatProperty(
+        name="Position",
+        description="Position of vertex",
+        min=0,
+        max=1,
+        step=1,
+        default=0.5
+    )
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.active_object
+        return obj is not None and obj.type == 'MESH'
+
+    def execute(self, context):
+        obj = context.active_object
+
+        me = obj.data
+        bme = bmesh.from_edit_mesh(me)
+
+        selected_edges = [e for e in bme.edges if e.select]
+
+        if len(selected_edges) < 1:
+            self.report({'INFO'}, 'No edges selected')
+            return {'CANCELLED'}
+
+        for edge in selected_edges:
+            # (new_edge, vert) =
+            bmesh.utils.edge_split(edge, edge.verts[0], self.position)
+            # edge.verts[0].select = False
+            # edge.verts[1].select = False
+            # new_edge.verts[0].select = False
+            # new_edge.verts[1].select = False
+            # vert.select = True
+
+        bmesh.update_edit_mesh(me)
+
+        # mode = obj.mode
+        # bpy.ops.object.mode_set(mode='OBJECT')
+        # bpy.ops.object.mode_set(mode=mode)
+
+        return {'FINISHED'}
+
+
+# endregion
+
+
+class PreciseToolsPanel(bpy.types.Panel):
+    """Precise Tools"""
+    bl_label = "Precise Tools"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Tool"
@@ -91,21 +144,36 @@ class OriginToolsPanel(bpy.types.Panel):
 
         row = layout.row()
         row.label(text="Active object is: {}".format(obj.name))
+
         row = layout.row()
-        row.operator(PutOriginToCenter.bl_idname)
+        row.separator()
 
+        row = layout.row()
+        row.label(text="Origin Tools")
 
-# endregion
+        row = layout.row()
+        row.operator(PutOriginToCenterOp.bl_idname)
+
+        row = layout.row()
+        row.separator()
+
+        row = layout.row()
+        row.label(text="Edges Tools")
+
+        row = layout.row()
+        row.operator(SplitEdgesOp.bl_idname)
 
 
 def register():
-    bpy.utils.register_class(OriginToolsPanel)
-    bpy.utils.register_class(PutOriginToCenter)
+    bpy.utils.register_class(PreciseToolsPanel)
+    bpy.utils.register_class(PutOriginToCenterOp)
+    bpy.utils.register_class(SplitEdgesOp)
 
 
 def unregister():
-    bpy.utils.unregister_class(PutOriginToCenter)
-    bpy.utils.unregister_class(OriginToolsPanel)
+    bpy.utils.unregister_class(SplitEdgesOp)
+    bpy.utils.unregister_class(PutOriginToCenterOp)
+    bpy.utils.unregister_class(PreciseToolsPanel)
 
 
 if __name__ == "__main__":
